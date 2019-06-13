0355b962dc9156451ca7b2769208945d14027ee3
GerogeLeon <leongeroge@gmail.com>
2019-06-13 17:30:58 +0800

    transfered here

---
+++ src/main/java/com/practice/pagehelper/pagehelper_practice/PagehelperPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.practice.pagehelper.pagehelper_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagehelperPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PagehelperPracticeApplication.class, args);
+    }
+}
